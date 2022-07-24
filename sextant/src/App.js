8d3b1e7a1d2e85a7c10e5343a252411f1e5dd1d7
Joanne <jl7105@nyu.edu>
2022-07-24 17:26:28 -0400

    Task 2 - Exhibit Component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Banner from './Banner';
+import Exhibit from './Exhibit';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Exhibit />
     </div>
   );
 }
---
+++ sextant/src/Exhibit.js
@@ -0,0 +1,16 @@
+import "./Exhibit.css"
+import React from 'react'
+
+function Exhibit() {
+  return (
+    <div className="exhibit">
+        <div className="exhibit_card"> user's public IP  </div>
+        <div className="exhibit_card"> latency information for Pylon
+ </div>
+        <div className="exhibit_card"> Card 3 </div>
+    
+    </div>
+  )
+}
+
+export default Exhibit
