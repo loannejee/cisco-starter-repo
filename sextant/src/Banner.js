f5a3310fb4557f00ee0fa64b7c7d40ddbf4b45f7
Joanne <jl7105@nyu.edu>
2022-07-24 23:16:30 -0400

    banner update

--- sextant/src/Banner.js
+++ sextant/src/Banner.js
@@ -10,7 +10,7 @@ class Banner extends Component {
                     src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Cisco_logo_blue_2016.svg/2560px-Cisco_logo_blue_2016.svg.png'
                     alt=''
                 />
-                <h1 className="banner_title">Sextant Dashboard</h1>
+                <div className="banner_title">Sextant Dashboard</div>
             </div>
         )
     }
